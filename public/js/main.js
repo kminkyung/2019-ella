2ec5e281e3ec2ad427b3c5c73c86bd85059fb509
kminkyung <mminkyung.k@gmail.com>
2019-10-17 13:14:19 +0900

    상단배너 js로 event 주기

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,38 @@
+/* Ready - 페이지가 로드될 때 일어날 일들 */
+// 상단 배너 가리기
+var topClose = $.cookie("top-banner-close");
+if(topClose) $(".top-banner").hide();
+
+
+/* header */
+/* header의 x 버튼 클릭 */
+// cookie를 사용하지 않고 그냥 닫을 때 -새로고침하면 보임.
+/* 
+$("#bt-top-close").click(function(){
+	$(this).parent().parent().slideUp(400);
+});
+*/
+// cookie 로 제한시간을 10분 줄 때 
+$("#bt-top-close").click(function(){
+	$(this).parent().parent().slideUp(400);
+	var d = new Date() //2019-10-17 wed 
+	d.setTime(d.getTime() + 10*60*1000);
+	console.log(d);
+	$.cookie("top-banner-close", true, {expires: d})
+});
+
+// 언어, 통화 선택
+$(".sel-top .fa-angle-down").click(function(){
+	$(this).next().stop().slideToggle(300);
+	$(this).toggleClass("fa-angle-down fa-angle-up");
+});
+
+$(".sel-top li").click(function(){
+	$(this).parent().parent().children(".sel-top-img").attr("src", $(this).children("img").attr("src"));
+	$(this).parent().parent().children(".sel-top-txt").text($(this).children("span").text());
+	$(this).parent().prev().trigger("click");
+});
+
 var $grid = $('.grid-wrap').imagesLoaded(function() {
   $grid.masonry({
 		itemSelector: '.grid-item',
--- public/js/util.js
+++ public/js/util.js
@@ -160,5 +160,35 @@ function pagerMaker($pager, grpCnt, divCnt, total, page, cb) {
 	html += '</li>';
 	$pager.html(html);
 	$(".page-item").css({"cursor":"pointer"});
-	$(".page-item").click(cb);
+	$(".page-item").click(cb); // gbook_ajax
+}
+$(".page-item").click(function(){
+	var n = $(this).data("page");
+	if(n !== undefined) location.href = $(".pager").data("pager-name")+n;
+});
+
+
+var imgExt = ["jpg", "jpeg", "png", "gif"];
+var fileExt = ["hwp", "doc", "docx", "xls", "xlsx", "ppt", "pptx", "txt", "zip", "pdf"];
+
+function splitName(file) {
+	var arr = file.split("."); 
+	var obj = {};
+	obj.ext = arr[1]; // timestamp+random.ext <- index1 
+	obj.name = arr[0]; // timestamp+random <- index0 .ext 
+	return obj;
+}
+
+function findPath(d) {
+	var year = String(d.getFullYear()).substr(2);
+	var month = d.getMonth() + 1; // 어차피 String인 year를 더할 것이므로 (숫자+문자=문자) String()을 하지 않았다.
+	if(month < 10) month = "0" + month; // 1~12 만들기, 만약 9가 10보다 작으면 0을 붙이고 month에 1을 더한다. 
+	return year + month; 
+}
+
+
+function telChk(obj){
+	if (String(obj.value).length > 4){
+		obj.value = obj.value.slice(0, 4);
+	}    
 }
