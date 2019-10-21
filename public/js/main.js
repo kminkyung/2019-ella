41e777151f68201dd45f26fb98a03593b24d61b3
kminkyung <mminkyung.k@gmail.com>
2019-10-21 14:43:01 +0900

    commit

--- public/js/main.js
+++ public/js/main.js
@@ -33,6 +33,26 @@ $(".sel-top li").click(function(){
 	$(this).parent().prev().trigger("click");
 });
 
+// 메인 네비게이션
+$(".navi-under").mouseenter(function(){
+	$(this).find(".subs").css("visibility","visible").stop().animate({"top": "43px", "opacity": 1}, 300);
+});
+$(".navi-under").mouseleave(function(){
+	$(this).find(".subs").stop().animate({"top": "143px", "opacity": 0}, 300, function(){
+		$(this).css("visibility","hidden");
+	})
+});
+$(".navi-show").mouseenter(function(){
+	$(this).find(".subs").css("visibility", "visible").stop().animate({"opacity": 1}, 300);
+});
+$(".navi-show").mouseleave(function(){
+	$(this).find(".subs").stop().animate({"opacity": 0}, 300, function(){
+		$(this).css("visibility", "hidden");
+	});
+});
+
+// 배너 (fade, slide(전체), slide(하나씩), slide(세로))
+
 var $grid = $('.grid-wrap').imagesLoaded(function() {
   $grid.masonry({
 		itemSelector: '.grid-item',
